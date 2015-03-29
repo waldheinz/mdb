9591ba343c4a5ba1d68d2bb7e6951534123780ee
Matthias Treydte <mt@waldheinz.de>
2015-03-29 19:48:13 +0200

    Added "added" timestamp to file table.

--- files/sql/create-table-file.sql
+++ files/sql/create-table-file.sql
@@ -1,8 +1,9 @@
 
-CREATE TABLE file
-    ( file_id   INTEGER PRIMARY KEY AUTOINCREMENT
-    , file_sha1 BINARY(20)
-    , file_name TEXT UNIQUE
-    , file_size INTEGER NOT NULL
-    , file_mime TEXT NOT NULL
+CREATE TABLE file2
+    ( file_id       INTEGER PRIMARY KEY AUTOINCREMENT
+    , file_sha1     BINARY(20)
+    , file_name     TEXT UNIQUE
+    , file_size     INTEGER NOT NULL
+    , file_mime     TEXT NOT NULL
+    , file_add_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
     );

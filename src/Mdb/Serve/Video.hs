444f4aa037693d977ca0215c9c3fbe3e9fd8f1d9
Matthias Treydte <mt@waldheinz.de>
2016-03-05 17:57:38 +0100

    serve: Can push videos through ffmpeg for HTML5 playback

--- src/Mdb/Serve/Video.hs
+++ src/Mdb/Serve/Video.hs
@@ -5,7 +5,13 @@ module Mdb.Serve.Video (
     videoApp
     ) where
 
+import           Blaze.ByteString.Builder ( fromByteString )
+import Blaze.ByteString.Builder.Char8 ( fromString )
 import           Control.Monad.IO.Class ( MonadIO, liftIO )
+import           Control.Monad.Trans.Class ( lift )
+import qualified Data.ByteString as BS
+import           Data.Conduit
+import           Data.Conduit.Process
 import           Network.HTTP.Types ( status200 )
 import           Network.Wai
 import           Network.Wai.Routing
@@ -15,11 +21,34 @@ import qualified Mdb.Database.File as DBF
 
 videoApp :: MediaDb -> Application
 videoApp mdb req respond = runMDB' mdb $ route root req (liftIO . respond) where
-    root = prepare $
-        get "/streamDirect/:id" (continue getStream)      $ capture "id"
+    root = prepare $ do
+        get "/stream/:id"       (continue stream)       $ capture "id"
+        get "/streamDirect/:id" (continue streamDirect) $ capture "id"
 
-getStream :: MonadIO m => DBF.FileId -> MDB m Response
-getStream fid = do
+streamDirect :: MonadIO m => DBF.FileId -> MDB m Response
+streamDirect fid = do
     f <- fileById fid
     p <- fileAbs $ DBF.filePath f
     return $ responseFile status200 [] p Nothing
+
+stream :: MonadIO m => DBF.FileId -> MDB m Response
+stream fid = do
+    f <- fileById fid
+    p <- fileAbs $ DBF.filePath f
+
+    let
+        cmd = "ffmpeg " ++
+            "-i \"" ++ p ++ "\" " ++
+            "-f matroska " ++
+            " - 2>/dev/null" :: String
+
+        str write flush = do
+            sourceCmdWithConsumer cmd $ do
+                awaitForever $ \bs -> do
+                    lift $ write (fromByteString bs)
+                    lift $ putStrLn $ show (BS.length bs, " bytes written")
+                    -- lift $ flush
+            flush
+
+    liftIO $ putStrLn $ "command is " ++ cmd
+    return $ responseStream status200 [] str

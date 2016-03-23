921925225764254728b61f579589615ba4e2c17e
Matthias Treydte <mt@waldheinz.de>
2016-03-23 19:40:56 +0100

    mdb: Add album.album_created column

--- files/sql/create-table-album.sql
+++ files/sql/create-table-album.sql
@@ -3,5 +3,6 @@ CREATE TABLE album
     ( album_id      INTEGER PRIMARY KEY AUTOINCREMENT
     , album_name    TEXT
     , album_poster  INTEGER
+    , album_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
     , FOREIGN KEY (album_poster) REFERENCES file(file_id)
     );

a41cb68d6a6d9a0cff1d05d84b47d4b1daf5dd9b
Matthias Treydte <mt@waldheinz.de>
2016-03-23 18:39:18 +0100

    mdb: Add NOT NULL constraint to file.file_name

--- files/sql/create-table-file.sql
+++ files/sql/create-table-file.sql
@@ -2,7 +2,7 @@
 CREATE TABLE file
     ( file_id       INTEGER PRIMARY KEY AUTOINCREMENT
     , file_sha1     BINARY(20)
-    , file_name     TEXT UNIQUE
+    , file_name     TEXT NOT NULL UNIQUE
     , file_size     INTEGER NOT NULL
     , file_mime     TEXT NOT NULL
     , file_add_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL

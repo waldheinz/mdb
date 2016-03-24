621181f0a0ddc4cd8125387177921d5a920edadd
Matthias Treydte <mt@waldheinz.de>
2016-03-24 13:52:09 +0100

    mdb: Add file_to_album SQL index to speed up albums-with-person listings

--- files/sql/create-table-album_file.sql
+++ files/sql/create-table-album_file.sql
@@ -6,3 +6,5 @@ CREATE TABLE album_file
     , FOREIGN KEY (album_id) REFERENCES album(album_id)
     , FOREIGN KEY (file_id) REFERENCES file(file_id)
     );
+
+CREATE INDEX file_to_album ON album_file(file_id, album_id);

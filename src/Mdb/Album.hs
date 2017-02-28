59d6ceec7d0bbd6485f00b3781f5fb4bfcd32682
Matthias Treydte <waldheinz@gmail.com>
2017-02-28 20:30:31 +0100

    Fix remove-empty-albums query

--- src/Mdb/Album.hs
+++ src/Mdb/Album.hs
@@ -20,7 +20,9 @@ doAlbum (AlbumCreate name) = isolate $ do
     dbExecute "INSERT INTO album (album_name) VALUES (?)" (SQL.Only name)
     dbLastRowId >>= \rid -> logInfoN $ "created with id " <> T.pack (show rid)
 doAlbum AlbumRemoveEmpty = isolate $ do
-    dbExecute_ $ "DELETE FROM album WHERE NOT EXISTS " <>
-        "(SELECT NULL FROM album_file af WHERE af.album_id = album_id)"
+    dbExecute_ $
+        "DELETE FROM album WHERE album_id IN " <>
+            "(select album_id from album a where not exists " <>
+                "(select null from album_file af where af.album_id = a.album_id))"
     cnt <- withConnection $ \c -> liftIO (SQL.changes c)
     logInfoN $ "deleted " <> T.pack (show cnt) <> " albums"

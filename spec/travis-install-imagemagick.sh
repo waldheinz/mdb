8f9b598fa5f7c2cdf30b500c44144a08c4ff70b3
Matthias Treydte <mt@waldheinz.de>
2017-06-11 14:16:56 +0200

    travis: Strip libwebp installation, we don't need it

--- spec/travis-install-imagemagick.sh
+++ spec/travis-install-imagemagick.sh
@@ -3,13 +3,7 @@
 convert -version | grep $IMAGEMAGICK_VERSION || {
   export CORES=$(nproc) &&
   echo "Using $CORES cores for compiling..." &&
-  cd /tmp &&
-  curl -O https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$LIBWEBP_VERSION.tar.gz &&
-  tar xvzf libwebp-$LIBWEBP_VERSION.tar.gz &&
-  cd libwebp-* &&
-  ./configure --prefix=$HOME/opt &&
-  make -j$CORES &&
-  make install -j$CORES &&
+
   cd /tmp &&
   curl -O https://www.imagemagick.org/download/ImageMagick-$IMAGEMAGICK_VERSION.tar.gz &&
   tar xvzf ImageMagick-$IMAGEMAGICK_VERSION.tar.gz &&
